6be0a82b294a5ba2581a8b9714420702732ad75e
2daw-Carlos <carlos.casado10@educa.madrid.org>
2026-01-12 17:46:41 +0100

    Primer commit

---
+++ src/main/java/es/carlos/tiendaalm/TiendaAlmApplication.java
@@ -0,0 +1,13 @@
+package es.carlos.tiendaalm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaAlmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiendaAlmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/carlos/tiendaalm/TiendaAlmApplicationTests.java
@@ -0,0 +1,13 @@
+package es.carlos.tiendaalm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaAlmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
